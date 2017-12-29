c9b5797180ed587276efd3c12ee75dd1757f90d1
AnandSundar <anandsundar96@gmail.com>
2017-12-29 04:05:12 -0500

    added loader gif and enhanced user experience

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 //listen for submit
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+  //hide results
+  document.getElementById('results').style.display = 'none';
+
+  //show loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 //calculate results
 
-function calculateResults (e){
+function calculateResults (){
   //UI Vars
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -25,12 +35,17 @@ function calculateResults (e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    //show results
+    document.getElementById('results').style.display = 'block';
+
+    //hide loader
+    document.getElementById('loading').style.display = 'none';
   }else{
     //error
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
 }
 
 function showError (error){
@@ -51,6 +66,12 @@ function showError (error){
 
   //clear error after 3 secs
   setTimeout(clearError, 3000);
+
+  //show results
+  document.getElementById('results').style.display = 'none';
+
+  //hide loader
+  document.getElementById('loading').style.display = 'none';
 }
 
 function clearError() {
